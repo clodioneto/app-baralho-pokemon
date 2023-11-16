31b325264a5dd36cf2987ea162bdb5bbb1e8a699
clodioneto <clodio.neto@edupass.io>
2023-11-15 23:43:02 -0300

    LazyLoad

--- src/app/home/home-route.module.ts
+++ src/app/home/home-route.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home.component';
+import { RouterModule, Routes } from '@angular/router';
+import { CartaComponent } from '../components/carta/carta.component';
+import { BaralhoComponent } from '../components/baralho/baralho.component';
 
-
+const routes: Routes = [
+  {
+    path:'',
+    component: HomeComponent,
+  },
+  {
+    path:'baralho',
+    component: BaralhoComponent,
+  },
+  {
+    path:'carta',
+    component: CartaComponent,
+  }
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
 })
 export class HomeRouteModule { }
